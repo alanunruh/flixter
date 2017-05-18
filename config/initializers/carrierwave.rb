c3bbf60b3b0634bd5cb831c6e3a6b4c8b4be5a8b
Alan Unruh <alan.unruh@gmail.com>
2017-05-18 11:45:58 +0000

    Video embedding

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,13 @@ CarrierWave.configure do |config|
   config.aws_acl = "public-read"
 
   config.aws_credentials = {
-    acess_key_id: ENV["AWS_ACCESS_KEY"],
+    access_key_id: ENV["AWS_ACCESS_KEY"],
     secret_access_key: ENV["AWS_SECRET_KEY"],
     region: ENV["AWS_REGION"]
   }
+
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
 end
