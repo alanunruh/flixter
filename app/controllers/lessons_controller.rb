e56bba9bfba58ddcc684b3c4498bc40e84e45c3c
Alan Unruh <alan.unruh@gmail.com>
2017-05-28 23:19:37 +0000

    Lesson controller permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,16 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_enrollment_for_current_course
 
   def show
   end
 
   private
+
+  def require_enrollment_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You Are Not Enrolled!"
+  end
 
   helper_method :current_lesson
   def current_lesson
